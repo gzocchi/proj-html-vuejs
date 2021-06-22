db59cb0cb0e3fe57f36373a320cf70f1a4fb90a1
gzocchi <z.giorgio.z@gmail.com>
2021-06-22 16:14:07 +0200

    modificato footer - aggiunto post e tag

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <Header :social="socialInfo" :navLink="headerLink" />
     <Jumbotron title="Brand New Arrivals" msg="New collection from New York" />
     <Main />
-    <Footer :social="socialInfo" :brand="brandInfo" :post="post" :tag="tag" />
+    <Footer :social="socialInfo" :brand="brandInfo" :post="post" :tags="tag" />
     <Banner />
   </div>
 </template>
@@ -155,57 +155,57 @@ export default {
         },
       },
       products: [],
-      post: {
-        post_1599_17_45_34: {
+      post: [
+        {
           title: "Aenean lobortis sapien enim viverra",
           date: "September 9th, 2015",
           text:
             "Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla,",
           comments: [],
         },
-        post_1599_14_30_12: {
+        {
           title: "Duis ac massa semper maximus",
           date: "September 9th, 2015",
           text:
             "Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla,",
           comments: [],
         },
-        post_1599_12_00_34: {
+        {
           title: "Nunc fermint nulla eu justo sem id",
           date: "September 9th, 2015",
           text:
             "Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla,",
           comments: [],
         },
-        post_1599_10_05_34: {
+        {
           title: "Aliquam posuere magna eget nibh",
           date: "September 9th, 2015",
           text:
             "Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla,",
           comments: [],
         },
-        post_1598_15_35_34: {
+        {
           title: "Aliquam posuere magna eget nibh",
           date: "September 8th, 2015",
           text:
             "Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla,",
           comments: [],
         },
-        post_1598_15_30_42: {
+        {
           title: "Cras ac nulla ac consecte rutrum",
           date: "September 8th, 2015",
           text:
             "Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla,",
           comments: [],
         },
-        post_1598_12_12_36: {
+        {
           title: "Fusce mattis nunc ut aliquam",
           date: "September 8th, 2015",
           text:
             "Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla,",
           comments: [],
         },
-      },
+      ],
       tag: [
         "Black",
         "boots",
@@ -242,5 +242,10 @@ export default {
   font-family: $font_family;
   font-size: $font_size;
   color: $shark;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
 }
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,10 +3,63 @@
     <section class="subscribe container-fluid">
       <Subscribe />
     </section>
-    <section class="info">
-      <h2>info</h2>
+    <section class="info container">
+      <div class="row py-5">
+        <div class="brand px-3 col-3">
+          <img
+            src="../assets/img/logo/classic_shop_logo_footer.png"
+            alt="Logo"
+            class="mb-2"
+          />
+          <ul class="m-0 p-0 py-4">
+            <li v-if="brand.street">
+              {{ brand.street }}
+            </li>
+            <li v-if="brand.city">
+              {{ brand.city }}
+            </li>
+            <li v-if="brand.state">{{ brand.state }}</li>
+            <li v-if="brand.phone">Phone: {{ brand.phone }}</li>
+            <li v-if="brand.mobile">Mobile: {{ brand.mobile }}</li>
+            <li v-if="brand.email">
+              Email: <a :href="`mailto:${brand.email}`">{{ brand.email }}</a>
+            </li>
+            <li v-if="brand.website">
+              Web: <a :href="brand.website" target="_blank">{{ web }}</a>
+            </li>
+          </ul>
+          <div class="social mt-2">
+            <a v-for="(item, index) in social" :key="index" :href="item.link">
+              <i :class="[item.icon]"></i>
+            </a>
+          </div>
+        </div>
+
+        <div class="products px-3 col-3">
+          <h6 class="text-uppercase">top rated products</h6>
+        </div>
+
+        <div class="posts px-3 col-3">
+          <h6 class="text-uppercase">recent posts</h6>
+          <ul class="m-0 p-0">
+            <li v-for="(item, index) in post" :key="index" class="py-2">
+              <i class="fas fa-chevron-right"></i>
+              <a href="#">{{ item.title }}</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="tags px-3 col-3">
+          <h6 class="text-uppercase">tags</h6>
+          <div>
+            <a v-for="(tag, index) in tags" :key="index" :href="`#${tag}`">
+              {{ tag }}
+            </a>
+          </div>
+        </div>
+      </div>
     </section>
-    <section class="copyright text-center py-2">
+    <section class="copyright container-fluid text-center py-2">
       <p>
         &copy; Copyright 2012-2020 | Avada Theme by Theme Fusion | All Rights
         Reserved | Powered by WordPress
@@ -24,7 +77,14 @@ export default {
   components: {
     Subscribe,
   },
-  props: ["social", "brand", "post", "tag"],
+  props: ["social", "brand", "post", "tags"],
+  computed: {
+    web() {
+      let website = this.brand.website.split(".");
+      website.splice(0, 1);
+      return website.join(".");
+    },
+  },
 };
 </script>
 
@@ -32,14 +92,65 @@ export default {
 @import "../assets/style/variables";
 
 footer {
+  font-weight: 500;
   color: $white;
-  background-color: #1d1f22;
+  background-color: #2b2e32;
 
   .subscribe {
     background-color: #303337;
   }
+
   .info {
-    background-color: #2b2e32;
+    font-size: 11px;
+
+    ul {
+      list-style: none;
+    }
+
+    h6 {
+      font-size: 12px;
+    }
+
+    .brand {
+      img {
+        height: 40px;
+      }
+      li {
+        margin: 5px 0;
+      }
+      .social {
+        a {
+          margin-right: 10px;
+        }
+      }
+    }
+
+    .products {
+      
+    }
+
+    .posts {
+      li {
+        border-bottom: 1px solid $fedora;
+        i {
+          margin-right: 5px;
+        }
+      }
+    }
+
+    .tags {
+      div {
+        display: flex;
+        flex-wrap: wrap;
+
+        a {
+          margin-right: 3px;
+          margin-bottom: 3px;
+          padding: 3px 4px;
+          border: 1px solid $fedora;
+        }
+      }
+    }
   }
   .copyright {
     background-color: #1d1f22;
